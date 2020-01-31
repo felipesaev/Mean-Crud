895c484793df1fd6ea120b9170c8deb25d3842f4
Felipe <felipesaev@gmail.com>
2020-01-31 07:43:05 -0300

    Subindo a aplicação do zero

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,27 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import {FuncAddComponent} from './func-add/func-add.component';
+import {FuncEditComponent} from './func-edit/func-edit.component';
+import {FuncGetComponent} from './func-get/func-get.component';
+import { componentFactoryName } from '@angular/compiler';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+{
+  path: 'funcionario',
+  component:FuncGetComponent
+
+},
+{
+  path:'funcionario/create',
+  component: FuncAddComponent
+  },
+  {
+  path: 'funcionario/edit/:id',
+  component: FuncEditComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
