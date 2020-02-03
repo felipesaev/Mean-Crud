3f0e56dbca7fa374e07c2610c9524c70daee0174
Felipe <felipesaev@gmail.com>
2020-02-03 07:53:54 -0300

    adicionando uma logica para o botao adicionar funcionario, com httpClient

--- src/app/func-add/func-add.component.ts
+++ src/app/func-add/func-add.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {FormGroup, FormBuilder,Validators} from '@angular/forms';
+import {FuncionarioService} from '../funcionario.service';
 
 @Component({
   selector: 'app-func-add',
@@ -10,7 +11,7 @@ export class FuncAddComponent implements OnInit {
 
   adicionarFuncionarioForm: FormGroup;
 
-  constructor(private formBuilder: FormBuilder) {
+  constructor(private formBuilder: FormBuilder, private funcionarioService: FuncionarioService) {
     this.createForm();
   }
   // Método responsavel por tratar as validações do Form que criará um novo Funcionario.
@@ -19,9 +20,14 @@ export class FuncAddComponent implements OnInit {
       nomeFuncionario:['', Validators.required],
       cargo: ['', Validators.required],
       numeroIdentificador: ['', Validators.required]
-    })
-
+    });
   }
+
+  //Método responsavel por adicionar um novo 'Funcionario' com ação do btn 'adicionar funcionario'
+
+  adicionarFuncionario(nomeFuncionario, cargo, numeroIdentificador) {
+    this.funcionarioService.adicionarFuncionario(nomeFuncionario, cargo, numeroIdentificador);
+    }
 
   ngOnInit() {
   }
--- src/app/funcionario.service.ts
+++ src/app/funcionario.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FuncionarioService {
+  uri = '/funcionario';
+  constructor(private http: HttpClient) { }
 
-  constructor() { }
+  //Método resposável por adicionar um novo "Funcionário" btn 'Adicionar Funcionário'.
+  adicionarFuncionario(nomeFuncionario, cargo, numeroIdentificador) {
+    const objfuncionario = {
+      nomeFuncionario: nomeFuncionario,
+      cargo: cargo,
+      numeroIdentificador: numeroIdentificador
+    };
+
+    console.log(objfuncionario);
+    this.http.post(`${this.uri}/add`, objfuncionario).subscribe(res=> console.log('OK'));
+  }
 }
