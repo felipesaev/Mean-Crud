c0a71fe64b12f19e79d371ac75d9434f4729d70a
Felipe <felipesaev@gmail.com>
2020-02-03 07:26:20 -0300

    criação do module service

---
+++ src/app/Funcionario.ts
@@ -0,0 +1,12 @@
+/**
+Arquivo : src/app/Funcionario.ts
+Data: 02/02/2020
+Descrição: Arquivo responsável pelo modelo de classe 'Funcionario'
+Autor: Felipe Costa.
+ */
+
+ export default class Funcionario {
+   nomeFuncionario: String;
+   cargo:String;
+   numeroIdentificador: Number;
+ }
